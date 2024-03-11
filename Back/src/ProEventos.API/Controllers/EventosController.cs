d51132f2e9793acde050236c2c41d00cbfb5e3a3
Julia Rauber <juliabrauber@gmail.com>
2024-03-11 16:20:29 -0300

    init

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EventosController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+          return  _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+          return  _context.Eventos.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+          return "exemplo";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+          return $"exemplo de put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+          return $"exemplo de put com id = {id}";
+        }
+    }
+}
