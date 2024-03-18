d54d88cbe84dc84803085edcbbb27a4adcde35fa
Julia Rauber <juliabrauber@gmail.com>
2024-03-18 11:08:36 -0300

    reestruturacao

---
+++ Back/src/ProEventos.Application/Contratos/IEventoService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Application.Contratos
+{
+    public interface IEventoService
+    {
+        Task<Evento> AddEventos(Evento model);
+        Task<Evento> UpdateEvento(int EventoId, Evento model);
+        Task<bool> DeleteEvento(int EventoId);
+
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
+        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes = false);
+
+    }
+}
--- Back/src/ProEventos.Persistence/Contexto/ProEventosContext.cs
+++ Back/src/ProEventos.Persistence/Contexto/ProEventosContext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ProEventos.Domain;
 
-namespace ProEventos.Persistence
+namespace ProEventos.Persistence.Contexto
 {
     public class ProEventosContext : DbContext
     {
