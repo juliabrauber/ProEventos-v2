4a020ba7d13d9ed7115ce23ebe1cd7839e4bdbdc
Julia Rauber <juliabrauber@gmail.com>
2024-03-21 15:48:20 -0300

    layout

--- Front/src/app/shared/titulo/titulo.component.ts
+++ Front/src/app/shared/titulo/titulo.component.ts
@@ -8,6 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TituloComponent implements OnInit {
 
   @Input() titulo!: string;
+  @Input() iconClass= 'fa fa-user';
+  @Input() subtitulo = 'Desde 2023';
+  @Input() botaoListar = false;
 
   constructor() { }
 
